01766f8088a4b9da50b1699205a41ecba629d623
周嘉富 <jiafu.zhou@hand-china.com>
2016-12-16 19:16:50 +0800

    ss

---
+++ src/main/java/hand/second/App.java
@@ -0,0 +1,13 @@
+package hand.second;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
